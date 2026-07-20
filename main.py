bdc20c8fcacbcd55bc63df162b9158b0d99f2409
MT-store <mt.blockspin@gmail.com>
2026-07-21 02:17:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import discord
 from discord.ext import commands
 import os
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
 intents = discord.Intents.all()
 
@@ -12,6 +27,8 @@ bot = commands.Bot(
 @bot.event
 async def on_ready():
     print(f"تم تشغيل البوت: {bot.user}")
+
+keep_alive()
 
 TOKEN = os.getenv("TOKEN")
 
